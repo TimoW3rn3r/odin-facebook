97ba24bef482f6d408755c6e94bdc53993603ad7
Werner <turbotimoh@gmail.com>
2023-06-05 13:17:18 +0545

    feat: seed database with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Delete all records
+User.delete_all
+Post.delete_all
 
 5.times do
   User.create!(
-    {
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
       address: Faker::Address.full_address,
       email: Faker::Internet.email,
       password: 'password'
-    }
+  )
+end
+
+lorems = [
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto veritatis ullam tenetur! Ad beatae tempore ab porro earum laboriosam distinctio. Ducimus quia perferendis fugiat ab ipsa sunt cupiditate expedita praesentium!",
+  "Obcaecati esse illum dolores amet excepturi maxime praesentium beatae ipsa, aperiam delectus nostrum dolor facilis sint at accusantium nisi architecto debitis labore laborum tempora soluta est magnam aut! Esse, nisi!",
+  "Tempora alias temporibus nemo atque molestias sequi ipsum maxime accusantium perferendis adipisci asperiores amet id culpa delectus dolorum, exercitationem laborum suscipit cumque est tempore fugit? Numquam ea vel fugit quae?",
+  "Facilis vero fugit vitae? Nobis exercitationem cumque quas deleniti, rerum quidem iusto autem numquam omnis, magnam vitae blanditiis maxime quos laboriosam vel quo praesentium, laudantium quae asperiores fugit dolorem! Cupiditate.",
+  "Dicta quod ea excepturi molestiae debitis dolores, aspernatur iusto officiis velit. Corporis, ad, error repellendus nostrum voluptate atque tempora esse porro blanditiis aliquid eum aliquam non dolor. Voluptas, quasi eligendi!",
+]
+
+user_ids = User.ids
+
+5.times do |i|
+  Post.create!(
+    body: lorems[i],
+    user_id: user_ids.sample
   )
 end
