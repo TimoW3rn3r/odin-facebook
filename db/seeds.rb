7773458732f522e62cb3bc06ba9137e590c82832
Werner <turbotimoh@gmail.com>
2023-06-05 12:54:56 +0545

    feat: seed database for users using Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  User.create!(
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      address: Faker::Address.full_address,
+      email: Faker::Internet.email,
+      password: 'password'
+    }
+  )
+end
