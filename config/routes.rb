589cf492b413116658027b2fd39bf6c6b5aa97e9
Werner <turbotimoh@gmail.com>
2023-06-06 10:07:02 +0545

    feat: add basic routes for post and user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts
+  resources :users, only: [:index, :show]
+
+  root 'users#index'
 end
