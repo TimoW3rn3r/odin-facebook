33e78ded93a40e550184f3d031b2e0a055a73512
Werner <turbotimoh@gmail.com>
2023-06-10 05:56:30 +0545

    feat: add feature to view users who liked the post by clicking on the likes count

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,10 @@
 class LikesController < ApplicationController
   before_action :set_likeable, only: [:create, :destroy]
 
+  def index
+    set_likeable_with_eager_load_likes
+  end
+
   def create
     @like = current_user.likes.build(
       likeable: @likeable
@@ -26,6 +30,12 @@ class LikesController < ApplicationController
   def set_likeable
     if params[:post_id]
       @likeable = Post.find(params[:post_id])
+    end
+  end
+
+  def set_likeable_with_eager_load_likes
+    if params[:post_id]
+      @likeable = Post.includes(likes: [:user]).find(params[:post_id])
     end
   end
 end
